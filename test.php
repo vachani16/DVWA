89152daa2edeeb179148bb2f9e5bb8c5b94f20a8
vedanshvachani <vedanshvachani16@gmail.com>
2026-02-19 19:27:25 +0530

    Update test.php

--- test.php
+++ test.php
@@ -4,4 +4,4 @@ $password = $_POST['password'];
 // Attacker can enter: ' OR '1'='1 for username to bypass login
 $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
 $result = mysqli_query($db_connection, $query);
-echo "";
+echo "hello";
